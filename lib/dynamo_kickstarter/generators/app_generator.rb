b2253d0c1c25f896a4581c81608d4f2a4c532a62
Nicholas Henry <nicholas.henry@gmail.com>
2012-08-17 12:56:42 -0400

    Add README template

--- lib/dynamo_kickstarter/generators/app_generator.rb
+++ lib/dynamo_kickstarter/generators/app_generator.rb
@@ -1,5 +1,12 @@
+require 'rails/generators'
+
 module DynamoKickstarter
-  class AppGenerator< Rails::Generators::NamedBase
+  class AppGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
+
+    def readme
+      remove_file 'README.rdoc'
+      template 'README.md.erb', 'README.md'
+    end
   end
 end
